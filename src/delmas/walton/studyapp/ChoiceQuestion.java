bed603fd9ab119eae28ad266dc1e195abb085608
Kayla Walton <fusco.k@gmail.com>
2017-09-18 20:02:45 -0700

    Initial update on ChoiceQuestion class. Adds choice options.

--- src/delmas/walton/studyapp/ChoiceQuestion.java
+++ src/delmas/walton/studyapp/ChoiceQuestion.java
@@ -1,5 +1,39 @@
 package delmas.walton.studyapp;
+import java.util.ArrayList;
 public class ChoiceQuestion extends Question
 {
+   private ArrayList<String> choices = new ArrayList<String>() ;
+   private String correctChoice;
 
+   /**
+    * construct a question with choices object
+    */
+   
+   /**
+    * Stores a choice and whether it is correct
+    * @param newChoice choice to be added
+    * @param truthValue is it correct?
+    */
+   public void addChoice(String newChoice, boolean truthValue)
+   {
+      if (truthValue)   //if it is the correct value, then mark it and add to list
+      {
+         correctChoice = newChoice;
+         choices.add(newChoice);
+      }
+      else
+      {
+         choices.add(newChoice);
+      }
+   }
+   /**
+    * Displays the question and all added choices
+    */
+   public void displayQuestionChoices()
+   {
+      for (int i = 0; i < choices.size(); i++) 
+      {
+      System.out.print(i + ": " + choices.get(i) + "\n");
+      }
+   }
 }
