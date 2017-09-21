a51ff4b78c843c39001aad9e4baf90497b8ebc2b
Kayla Walton <fusco.k@gmail.com>
2017-09-21 15:53:48 -0700

    Added ChoiceQuestionTest implementation and edited ChoiceQuestion

---
+++ src/delmas/walton/studyapp/ChoiceQuestionTest.java
@@ -0,0 +1,38 @@
+package delmas.walton.studyapp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class ChoiceQuestionTest
+{
+
+   @Test
+   public void test()
+   {
+      //create a choice question
+      ChoiceQuestion firstQ = new ChoiceQuestion("Who is the Dark Knight?", "Batman", true);
+      //test to see if display is not null
+      assertNotNull(firstQ.displayQuestion());
+      assertNotNull(firstQ.displayQuestionChoices());
+      //check if first correct answer stored properly
+      assertEquals(firstQ.checkAnswer("Batman"), true);
+      //second test for multiple choice insertion
+      
+      ChoiceQuestion secondQ = new ChoiceQuestion("Who is Wonder Woman?", "Gal Gadot", true);
+      
+      //store wrong answers
+      secondQ.addChoice("Vera Wang", false);
+      secondQ.addChoice("Barbara Gordon", false);
+      secondQ.addChoice("Sarah Palin", false);
+      //verify answers are in list
+      assertEquals("Choice check", "Vera Wang", secondQ.getChoices().get(1));
+      assertEquals("Second choice", "Barbara Gordon", secondQ.getChoices().get(2));
+      assertEquals("Third choice", "Sarah Palin", secondQ.getChoices().get(3));
+      //verify whole list of answers will display
+      assertNotNull(secondQ.displayQuestionChoices());
+   }
+  
+
+}
