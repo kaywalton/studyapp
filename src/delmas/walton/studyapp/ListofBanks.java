621e2e271b8b0b59dcbe804a8f980e6a144686de
Isabelle Delmas <isabelle.lenertz@gmail.com>
2017-10-06 18:17:50 -0700

    added getBank(String)

--- src/delmas/walton/studyapp/ListofBanks.java
+++ src/delmas/walton/studyapp/ListofBanks.java
@@ -11,16 +11,39 @@ public class ListofBanks implements Serializable{
 	private static final long serialVersionUID = -1554421568867838164L;
 	private ArrayList<QuestionBank> list;
 	
+	/**
+	 * Default constructor
+	 */
 	public ListofBanks() {
 		this.list = new ArrayList<QuestionBank>();
 	}
 	
+	/**
+	 * Get a bank of question
+	 * @param index : where the bank is in the list
+	 * @return : a reference to the bank
+	 */
 	public QuestionBank getBank(int index) {
 		if (index < this.list.size()) {
 			return this.list.get(index);
 		} else {
 			return null;
 		}
+	}
+	
+	/**
+	 * Get a bank of question
+	 * @param bankName : name of the bank in the list
+	 * @return : a reference to the bank
+	 */
+	public QuestionBank getBank(String bankName) {
+		QuestionBank bank = null;
+		for(int i = 0; i < this.list.size(); i++) {
+			if(this.list.get(i).getName().equals(bankName)) {
+				bank = this.list.get(i);
+			}
+		}
+		return bank;
 	}
 	
 	public void setBank(QuestionBank bank) {
